f863db789cc464c9c1f84a6b08d98c2e64fb10e1
WPT <wenoateves@gmail.com>
2023-09-28 21:46:03 +0100

    parameter + cursor update

--- script.js
+++ script.js
@@ -7,39 +7,11 @@ let isHovering = false;
 
 imageContainer.addEventListener('mouseenter', () => {
     isHovering = true;
-    parameterDisplay.style.display = 'block';
+    parameterDisplay.textContent = "beagle!";
+
 });
 
 imageContainer.addEventListener('mouseleave', () => {
     isHovering = true;
-    parameterDisplay.style.display = 'none';
+    parameterDisplay.textContent = "bagel...";
 });
-
-/*
-// Create a temporary canvas element
-const tempCanvas = document.createElement('canvas');
-const ctx = tempCanvas.getContext('2d');
-
-// Draw the image onto the canvas
-tempCanvas.width = hoverImage.width;
-tempCanvas.height = hoverImage.height;
-ctx.drawImage(hoverImage, 0, 0, tempCanvas.width, tempCanvas.height);
-
-imageContainer.addEventListener('mousemove', (e) => {
-    const rect = tempCanvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    
-    // Get the pixel data of the mouse position on the canvas
-    const pixel = ctx.getImageData(x, y, 1, 1).data;
-    
-    // Check if the pixel is transparent
-    if (pixel[3] !== 0) {
-        imageContainer.style.cursor = 'pointer'; // Change cursor style
-        console.log("hey")
-    } else {
-        imageContainer.style.cursor = 'default'; // Reset cursor style
-    }
-
-});
-*/
